a65c9847604fce8bd242201579abe85f390ed453
David Guttman <david@davidguttman.com>
2021-04-18 22:21:01 -0700

    feat:add user-agent endpoint

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") return respondHello(req, res);
+<<<<<<< HEAD
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
   if (req.url === "/") return respondHello(req, res);
   if (req.url === "/user-agent") return respondUserAgent(req, res);
+=======
+  if (req.url === "/user-agent") return respondUserAgent(req, res);
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
+  if (req.url === "/") return respondHello(req, res);
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
+>>>>>>> 2566d72 (feat:add user-agent endpoint)
 
   res.end();
 });
@@ -14,21 +21,8 @@ const server = http.createServer((req, res) => {
 function respondHello(req, res) {
   res.end(JSON.stringify({ msg: "hello" }));
 }
-function respondBase64(req, res) {
-  const phrase = req.url.replace(/^\/b64\//, "");
-  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
-}
-server.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
 
-if (require.main !== module) module.exports = server;
-function respondUserAgent(req, res) {
-  const ua = req.headers["user-agent"];
-  res.end(JSON.stringify({ ua }));
-}
 
-server.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
 function respondUserAgent(req, res) {
   const ua = req.headers["user-agent"];
   res.end(JSON.stringify({ ua }));
@@ -37,5 +31,11 @@ function respondBase64(req, res) {
   const phrase = req.url.replace(/^\/b64\//, "");
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
 }
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, "");
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
+}
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
 
 if (require.main !== module) module.exports = server;
