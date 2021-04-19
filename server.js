fda04e67e091e1ecc1dd447e65ae732f1ad8208b
David Guttman <david@davidguttman.com>
2021-04-18 22:22:44 -0700

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -29,5 +29,13 @@ function respondUserAgent(req, res) {
 
 server.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
+function respondUserAgent(req, res) {
+  const ua = req.headers["user-agent"];
+  res.end(JSON.stringify({ ua }));
+}
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, "");
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
+}
 
 if (require.main !== module) module.exports = server;
