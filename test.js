cfbcfa88bccc35ea18d42374191621d8300099df
Akshay Sharma <akshaysharma@Akshays-MacBook-Pro.local>
2022-09-27 11:49:07 +0530

    init

--- test.js
+++ test.js
@@ -29,3 +29,17 @@ tape("cleanup", function (t) {
   server.close();
   t.end();
 });
+tape("should respond user-agent", (t) => {
+  const opts = { headers: { "User-Agent": "tape" } };
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err);
+
+    t.equal(body.ua, "tape");
+    t.end();
+  });
+});
+
+tape("cleanup", function (t) {
+  server.close();
+  t.end();
+});
